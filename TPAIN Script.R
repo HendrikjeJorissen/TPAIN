643ac3c0f0b3bdd3b73a23b3e3f0c27e0cd26891
Hendrikje Jorissen <hendrikjejorissen@gmail.com>
2021-02-16 18:37:42 -1000

    TPAIN

--- TPAIN Script.R
+++ TPAIN Script.R
@@ -34,3 +34,37 @@ glimpse(photo.data)
 #decide color scheme for the plots
 #cols<-c("#99817b", "#F2C3A7", "#FEF3E1", "#C489B9")
 cols<-c("#073e3e", "#c35119", "#f896b0", "#e4e0ca")
+
+mydata <- photo.data
+
+# convert temp to K
+mydata$K<-mydata$Temp.C + 273.15
+
+#define the Sharpe-Schoolfield equation
+schoolfield_high <- function(lnc, E, Eh, Th, temp, Tc) {
+  Tc <- 273.15 + Tc
+  k <- 8.62e-5
+  boltzmann.term <- lnc + log(exp(E/k*(1/Tc - 1/temp))) #units are eV/K, electrovolts/Kelvin
+  inactivation.term <- log(1/(1 + exp(Eh/k*(1/Th - 1/temp))))
+  return(boltzmann.term + inactivation.term)
+}
+
+# fit over each set of groupings
+
+#droplevels(mydata$Organism.ID) ###DOES NOT WORK ###WHAT DOES THIS DO?
+mydata$Treatment<-as.character(mydata$Treatment)
+mydata$Organism.ID<-as.character(mydata$Organism.ID)
+
+fits <- mydata %>%
+  group_by(Organism.ID, Treatment) %>%
+  nest() %>%
+  mutate(fit = purrr::map(data, ~ nls_multstart(log.rate ~ schoolfield_high(lnc, E, Eh, Th, temp = K, Tc = 26),
+                                                data = .x,
+                                                iter = 1000,
+                                                start_lower = c(lnc = -10, E = 0.1, Eh = 0.2, Th = 285),
+                                                start_upper = c(lnc = 10, E = 2, Eh = 5, Th = 330),
+                                                supp_errors = 'Y',
+                                                na.action = na.omit,
+                                                lower = c(lnc = -10, E = 0, Eh = 0, Th = 0))))
+
+
